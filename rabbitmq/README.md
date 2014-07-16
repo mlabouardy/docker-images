922ead56c3a145d72ea50e95a34d96d4522d39e1
Fawad Halim <fawad@fawad.net>
2014-07-15 23:18:01 -0500

    Use WORKDIR instead of cd

--- rabbitmq/README.md
+++ rabbitmq/README.md
@@ -6,16 +6,16 @@ Docker container for RabbitMQ 3.3.x with ability to form cluster
 Usage
 =====
 
-Start a Serf master using
+Start a [consul](http://consol.io/) node using
 
-`SERF_ID=$(docker run -d --name serf_1 -p 7946 -p 7373 ctlc/serf /run.sh)`
+`docker run --name=consul -p 8500:8500 -p 8600:8600/udp -p 8400:8400 fhalim/consul`
 
 Master
 ------
 
-`docker run -p 5672:5672 -p 15672:15672 --name="rmq1" fhalim/rabbitmq`
+`docker run --link=consul:consul -p 5672:5672 -p 15672:15672 --name="rmq1" fhalim/rabbitmq`
 
 Additional nodes
 ----------------
 
-`docker run --link=rmq1:master fhalim/rabbitmq`
+`docker run --link=consul:consul --link=rmq1:master fhalim/rabbitmq`
--- rabbitmq/rabbitmq-startup.sh
+++ rabbitmq/rabbitmq-startup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-serf join $SERF_1_PORT_7946_TCP_ADDR:$SERF_1_PORT_7946_TCP_PORT
 
+CONSUL_IP="${CONSUL_PORT_8500_TCP_ADDR}"
 MASTER_IP="${MASTER_PORT_5672_TCP_ADDR}"
 MASTER_HOSTNAME="master"
 
 MASTER_NODENAME="rabbit@${MASTER_IP}"
+MYIP=$( ip a s|grep 'inet '|grep -v '127.0.0.1'|awk '{print $2;}'|awk 'BEGIN {FS="/"} {print $1;}')
 if [ -z $MASTER_IP ]; then
-	MYIP=$( ip a s|grep 'inet '|grep -v '127.0.0.1'|awk '{print $2;}'|awk 'BEGIN {FS="/"} {print $1;}')
-	MYNODENAME="rabbit@${MYIP}"
+	MYNODENAME="rabbit@${MASTER_HOSTNAME}"
 	echo "Running RabbitMQ server as standalone node using nodename ${MYNODENAME}.";
-	cp /usr/lib/rabbitmq/bin/rabbitmq-server /usr/lib/rabbitmq/bin/rabbitmq-server-copy
-	sed -i 's/sname ${RABBITMQ_NODENAME}/name ${RABBITMQ_NODELONGNAME}/' /usr/lib/rabbitmq/bin/rabbitmq-server
-	export RABBITMQ_NODELONGNAME="${MYNODENAME}"
+	#cp /usr/lib/rabbitmq/bin/rabbitmq-server /usr/lib/rabbitmq/bin/rabbitmq-server-copy
+	#sed -i 's/sname ${RABBITMQ_NODENAME}/name ${RABBITMQ_NODELONGNAME}/' /usr/lib/rabbitmq/bin/rabbitmq-server
+	export RABBITMQ_NODENAME="${MYNODENAME}"
 	rabbitmq-server
 	/bin/bash
 else
