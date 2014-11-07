7531452431c2687bc7d2a1d0c69043b1338e690b
Fawad Halim <fawad@fawad.net>
2014-11-06 20:41:32 -0600

    Take the easy way out and name the first node master

--- rabbitmq/README.md
+++ rabbitmq/README.md
@@ -1,21 +1,19 @@
 Introduction
 ============
 
-Docker container for RabbitMQ 3.3.x with ability to form cluster
+Docker container for RabbitMQ 3.4.x with ability to form cluster
 
 Usage
 =====
 
-Start a [consul](http://consol.io/) node using
-
-`docker run --name=consul -p 8500:8500 -p 8600:8600/udp -p 8400:8400 fhalim/consul`
+Uses the ability in recent Docker versions to set up hosts entries to locate a node to cluster with. For
+convenience sake, the node is named master.
 
 Master
 ------
 
-`docker run --link=consul:consul -p 5672:5672 -p 15672:15672 --name="rmq1" fhalim/rabbitmq`
-
+`docker run -d --name rmqmaster --add-host master:127.0.0.1 -p 5672:5672 -p 15672:15672 fhalim/rabbitmq`
 Additional nodes
 ----------------
 
-`docker run --link=consul:consul --link=rmq1:master fhalim/rabbitmq`
+`docker run -d --link=rmqmaster:master fhalim/rabbitmq`
--- rabbitmq/rabbitmq-startup.sh
+++ rabbitmq/rabbitmq-startup.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
+MASTER_HOSTNAME="${MASTER_NAME}"
 
-CONSUL_IP="${CONSUL_PORT_8500_TCP_ADDR}"
-MASTER_IP="${MASTER_PORT_5672_TCP_ADDR}"
-MASTER_HOSTNAME="master"
-
-MASTER_NODENAME="rabbit@${MASTER_IP}"
-MYIP=$( ip a s|grep 'inet '|grep -v '127.0.0.1'|awk '{print $2;}'|awk 'BEGIN {FS="/"} {print $1;}')
-if [ -z $MASTER_IP ]; then
-	MYNODENAME="rabbit@${MASTER_HOSTNAME}"
-	echo "Running RabbitMQ server as standalone node using nodename ${MYNODENAME}.";
-	#cp /usr/lib/rabbitmq/bin/rabbitmq-server /usr/lib/rabbitmq/bin/rabbitmq-server-copy
-	#sed -i 's/sname ${RABBITMQ_NODENAME}/name ${RABBITMQ_NODELONGNAME}/' /usr/lib/rabbitmq/bin/rabbitmq-server
-	export RABBITMQ_NODENAME="${MYNODENAME}"
+MASTER_NODENAME="rabbit@master"
+if [ -z $MASTER_HOSTNAME ]; then
+	echo "Running RabbitMQ server as standalone node using nodename ${MASTER_NODENAME}.";
+	export RABBITMQ_NODENAME="${MASTER_NODENAME}"
 	rabbitmq-server
-	/bin/bash
 else
-	echo "Joining cluster to ${MASTER_IP}"
+	echo "Joining cluster to ${MASTER_NODENAME}"
 	rabbitmq-server -detached
+	P=$?
+	echo "PID: ${P}"
 	rabbitmqctl stop_app
-	echo "${MASTER_IP} ${MASTER_HOSTNAME}" >> /etc/hosts
-	rabbitmqctl join_cluster ${MASTER_NODENAME}
-	rabbitmqctl stop
-	rabbitmq-server
+	rabbitmqctl reset
+	rabbitmqctl join_cluster "${MASTER_NODENAME}"
+	rabbitmqctl start_app
+	cat /dev/zero
 fi;
